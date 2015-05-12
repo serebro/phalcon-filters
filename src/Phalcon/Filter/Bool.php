b54f9a813856ecaee71a08076010a83560282358
Sergey <sergilyin@gmail.com>
2015-05-12 16:27:46 +0300

    Update Bool.php

--- src/Phalcon/Filter/Bool.php
+++ src/Phalcon/Filter/Bool.php
@@ -7,17 +7,6 @@ class Bool
 
     public function filter($value)
     {
-        if (is_string($value)) {
-            $value = strtolower($value);
-        }
-
-        return 
-            $value === true || 
-            $value === 'true' || 
-            $value === '1' || 
-            $value === 1 || 
-            $value === 'y' || 
-            $value === 'yes' || 
-            $value === 'on';
+        return filter_var($value, FILTER_VALIDATE_BOOLEAN);
     }
 }
